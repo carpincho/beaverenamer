d4f708b048711d6e20ec3ef6ff40313e560ffd4c
Joaquin Tita <carpincho@gmail.com>
2011-11-06 16:25:04 -0300

    Added some filtering/renaming functions.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 import os
+import re
 
-PATH_TO_DIR = "."
-extention_allowed = ['.mp4', '.avi', '.srt']
+PATH_TO_DIR = "/Users/carpincho/Desktop/series/ITCrowd/temporada 2/"
+extension_allowed = ['.mp4', '.avi', '.srt']
+
+
+def get_name_from_filename(filename):
+    """
+    Get name without file extension.
+    """
+    return filename[:-4]
+
+
+def get_extension_from_filename(filename):
+    """
+    Get extension from filename.
+    """
+    return filename[-4:]
 
 
 def get_accepted_and_ommited_files(files):
@@ -10,7 +25,7 @@ def get_accepted_and_ommited_files(files):
     Splits allowed files from not allowed.
     Returns: ([allowed files],[not_allowed files])
     """
-#    assert len(files) > 0, "ERROR: get_accepted_and_ommited_files(). No files"
+    assert len(files) > 0, "ERROR: get_accepted_and_ommited_files(). No files"
     allowed = []
     not_allowed = []
     for file in files:
@@ -21,17 +36,75 @@ def get_accepted_and_ommited_files(files):
     return (allowed, not_allowed)
 
 
-def is_file_allowed(file):
+def is_file_allowed(filename):
     """
     Determine if a file is allowed to be renamed.
     """
     allowed = False
-    file_ext = file[-4:]
-    for ext in extention_allowed:
+    file_ext = get_extension_from_filename(filename)
+    for ext in extension_allowed:
         if file_ext == ext:
             allowed = True
             break
     return allowed
+
+
+def remove_extra_spaces(filename):
+    """
+    Remove doble spaces from 'filename'.
+    """
+    name = get_name_from_filename(filename)
+    ext = get_extension_from_filename(filename)
+
+    new_name = ' '.join(name.split())
+
+    return new_name + ext
+
+
+def replace_underscore(filename):
+    """
+    Replace all '_' with a space from 'filename'.
+    """
+    name = get_name_from_filename(filename)
+    ext = get_extension_from_filename(filename)
+    old, new = '_', ' '
+
+    new_name = name.replace(old, new)
+
+    return new_name + ext
+
+
+def replace_dots(filename):
+    """
+    Replace all '.' with space from 'filename'.
+    """
+    name = get_name_from_filename(filename)
+    ext = get_extension_from_filename(filename)
+    old, new = ".", " "
+
+    new_name = name.replace(old, new)
+
+    return new_name + ext
+
+
+def lower_case(filename):
+    """
+    Convert filaname to lower. This include the extension too
+    """
+    return filename.lower()
+
+
+def remove_url_patterns(filename, pattern):
+    """
+    Remove pattern from filename."
+    """
+    name = get_name_from_filename(filename)
+    ext = get_extension_from_filename(filename)
+    repl = " "
+    new_name = re.sub(pattern, repl, name)
+
+    return new_name + ext
+
 
 if __name__ == '__main__':
 
